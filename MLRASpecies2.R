88180f0252e0d1256020b6830fe7b8950e14fd4e
Greg Schmidt <12001001973813@FEDIDCARD.GOV>
2020-04-21 16:39:46 -0400

    more analysis

--- MLRASpecies2.R
+++ MLRASpecies2.R
@@ -19,21 +19,47 @@ distbray <- readRDS('data/usfssubsec.distbray.RDS')
 distjac <- readRDS('data/usfssubsec.distjac.RDS')
 distsim <- readRDS('data/usfssubsec.distsim.RDS')
 
+#betasim
+betasim <- function(p){
+  d <- matrix(1, nrow = nrow(p), ncol = nrow(p))
+  rownames(d) <- rownames(p)
+  colnames(d) <- rownames(p)
+  for(j in 1:nrow(p)){
+    for(k in 1:nrow(p)){
+      d[j,k] <- 1-sum((p[j,]*p[k,])^0.5)/min(sum(p[j,]), sum(p[k,]))
+    }}
+  d<-as.dist(d)
+}
+betasim2 <- function(p){
+  d <- matrix(1, nrow = nrow(p), ncol = nrow(p))
+  rownames(d) <- rownames(p)
+  colnames(d) <- rownames(p)
+  for(j in 1:nrow(p)){
+    for(k in 1:nrow(p)){
+      d[j,k] <- 1-sum((p[j,]*p[k,])^0.5)/sqrt(sum(p[j,])*sum(p[k,]))
+    }}
+  d<-as.dist(d)
+}
+
 
 #validity using silhouette index
 #----
 #distbray <- vegdist(plotdata, method='bray', binary=FALSE, na.rm=T)
 #distjac <- vegdist(plotdata, method='jaccard', binary=FALSE, na.rm=T)
 #distsim <- as.dist(simil(plotdata,method='Simpson'))
+#distbeta <- betasim(plotdata)
+distbeta2 <- betasim2(plotdata)
 #saveRDS(distbray,'data/usfssubsec.distbray.RDS')
 #saveRDS(distjac,'data/usfssubsec.distjac.RDS')
 #saveRDS(distsim,'data/usfssubsec.distsim.RDS')
+#saveRDS(distbeta,'data/usfssubsec.distbeta.RDS')
+saveRDS(distbeta2,'data/usfssubsec.distbeta2.RDS')
 
 k <- 2
 klevel <- 0
 sil.bray <- 0
 sil.jac <- 0
-sil.sim <- 0
+sil.beta <- 0
 sil.ward <- 0
 sil.diana <- 0
 sil.kmeans <- 0
@@ -44,7 +70,7 @@ sil.kmeanseuc <- 0
 for (k in 2:24){
   sil.bray1 <- (distbray %>% agnes(method = 'average') %>% cutree(k=k) %>% silhouette(distbray))[,3]%>% mean
   sil.jac1 <- (distjac %>% agnes(method = 'average') %>% cutree(k=k) %>% silhouette(distbray))[,3]%>% mean
-  sil.sim1 <- (distsim %>% agnes(method = 'average') %>% cutree(k=k) %>% silhouette(distbray))[,3]%>% mean
+  sil.beta1 <- (distsim %>% agnes(method = 'average') %>% cutree(k=k) %>% silhouette(distbray))[,3]%>% mean
   sil.ward1 <- (distbray %>% agnes(method = 'ward') %>% cutree(k=k) %>% silhouette(distbray))[,3]%>% mean
   sil.diana1 <- (distbray %>% diana %>% cutree(k=k) %>% silhouette(distbray))[,3]%>% mean
   sil.kmeans1 <- (kmeans(distbray, centers = k)$cluster %>% silhouette(distbray))[,3] %>% mean
@@ -61,7 +87,7 @@ for (k in 2:24){
   sil.jdiana <- c(sil.jdiana, sil.jdiana1)
   sil.complete <- c(sil.complete, sil.complete1)
 }
-sil.table <- as.data.frame(cbind(klevel,sil.bray,sil.jac,sil.sim,sil.ward,sil.diana,sil.kmeans,sil.jdiana,sil.complete))
+sil.table <- as.data.frame(cbind(klevel,sil.bray,sil.jac,sil.beta,sil.ward,sil.diana,sil.kmeans,sil.jdiana,sil.complete))
 sil.table <- sil.table[-1,]
 #saveRDS(sil.table,'output/usfssubsec.sil.table.RDS')
 
@@ -69,10 +95,8 @@ sil.table <- sil.table[-1,]
 k.agnes <- c(2,3,4,7,10,16,24)
 k.ward <- c(2,3,4,6,11,16,24)
 k.diana <- c(2,3,4,7,10,16,24)
-i<-1
-agnes.cut <- 0
-ward.cut <- 0
-diana.cut <- 0
+
+
 
 agnes.cut1<- distbray %>% agnes(method = 'average') %>% cutree(k=k.agnes[1])
 ward.cut1 <- distbray %>% agnes(method = 'ward') %>% cutree(k=k.ward[1])
@@ -95,11 +119,19 @@ diana.cut6 <- distbray %>% diana %>% cutree(k=k.diana[6])
 agnes.cut7<- distbray %>% agnes(method = 'average') %>% cutree(k=k.agnes[7])
 ward.cut7 <- distbray %>% agnes(method = 'ward') %>% cutree(k=k.ward[7])
 diana.cut7 <- distbray %>% diana %>% cutree(k=k.diana[7])
+simp.cut1<- distsim %>% agnes(method = 'average') %>% cutree(k=k.agnes[1])
+simp.cut2<- distsim %>% agnes(method = 'average') %>% cutree(k=k.agnes[2])
+simp.cut3<- distsim %>% agnes(method = 'average') %>% cutree(k=k.agnes[3])
+simp.cut4<- distsim %>% agnes(method = 'average') %>% cutree(k=k.agnes[4])
+simp.cut5<- distsim %>% agnes(method = 'average') %>% cutree(k=k.agnes[5])
+simp.cut6<- distsim %>% agnes(method = 'average') %>% cutree(k=k.agnes[6])
+simp.cut7<- distsim %>% agnes(method = 'average') %>% cutree(k=k.agnes[7])
 
 
 usfsclust <- as.data.frame(cbind(agnes.cut1,agnes.cut2,agnes.cut3,agnes.cut4,agnes.cut5,agnes.cut6,agnes.cut7,
                    ward.cut1,ward.cut2,ward.cut3,ward.cut4,ward.cut5,ward.cut6,ward.cut7,
-                   diana.cut1,diana.cut2,diana.cut3,diana.cut4,diana.cut5,diana.cut6,diana.cut7))
+                   diana.cut1,diana.cut2,diana.cut3,diana.cut4,diana.cut5,diana.cut6,diana.cut7,
+                   simp.cut1,simp.cut2,simp.cut3,simp.cut4,simp.cut5,simp.cut6,simp.cut7))
 usfsclust$subsect <- rownames(usfsclust)
 
 write.dbf(usfsclust, 'output/usfsclust.dbf')
@@ -214,3 +246,44 @@ coph.sim
 coph.wardsim
 coph.dianasim
 coph.wardjaccard
+
+
+
+silanalysis2 <- function(input){
+  #distbray <- vegdist(input, method='bray', binary=FALSE, na.rm=T)
+  #distjac <- vegdist(input, method='jaccard', binary=FALSE, na.rm=T)
+  #distsim <- as.dist(simil(input,method='Simpson'))
+  
+  maxcluster <- min(24, nrow(input)-1)
+  k <- 2
+  klevel <- 0
+  sil.bray <- 0
+  sil.jac <- 0
+  sil.sim <- 0
+  sil.ward <- 0
+  sil.diana <- 0
+  
+  for (k in 2:maxcluster){
+    sil.bray1 <- (distbray %>% agnes(method = 'average') %>% cutree(k=k) %>% silhouette(distbray))[,c(1,3)] %>% as.matrix()%>%as.data.frame() 
+    sil.bray1 <- (aggregate(sil.bray1[,2], by=list(sil.bray1[,1]), FUN='mean'))[,2] %>% mean()
+    sil.jac1 <- (distjac %>% agnes(method = 'average') %>% cutree(k=k) %>% silhouette(distbray))[,c(1,3)] %>% as.matrix()%>%as.data.frame() 
+    sil.jac1 <- (aggregate(sil.jac1[,2], by=list(sil.jac1[,1]), FUN='mean'))[,2] %>% mean()
+    sil.sim1 <- (distsim %>% agnes(method = 'average') %>% cutree(k=k) %>% silhouette(distbray))[,c(1,3)] %>% as.matrix()%>%as.data.frame() 
+    sil.sim1 <- (aggregate(sil.sim1[,2], by=list(sil.sim1[,1]), FUN='mean'))[,2] %>% mean()
+    sil.ward1 <- (distbray %>% agnes(method = 'ward') %>% cutree(k=k) %>% silhouette(distbray))[,c(1,3)] %>% as.matrix()%>%as.data.frame() 
+    sil.ward1 <- (aggregate(sil.ward1[,2], by=list(sil.ward1[,1]), FUN='mean'))[,2] %>% mean()
+    sil.diana1 <- (distbray %>% diana %>% cutree(k=k) %>% silhouette(distbray))[,c(1,3)] %>% as.matrix()%>%as.data.frame() 
+    sil.diana1 <- (aggregate(sil.diana1[,2], by=list(sil.diana1[,1]), FUN='mean'))[,2] %>% mean()
+    
+    klevel <- c(klevel, k)
+    sil.bray <- c(sil.bray, sil.bray1)
+    sil.jac <- c(sil.jac, sil.jac1)
+    sil.sim <- c(sil.sim, sil.sim1)
+    sil.ward <- c(sil.ward, sil.ward1)
+    sil.diana <- c(sil.diana, sil.diana1)
+  }
+  sil.table <- as.data.frame(cbind(klevel,sil.bray,sil.jac,sil.sim,sil.ward,sil.diana))
+  sil.table <- sil.table[-1,]
+  sil.table2 <<- sil.table
+  return(sil.table)}
+silanalysis2(plotdata)
